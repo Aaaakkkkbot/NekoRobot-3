f17c242e3c100f186530a6dbe05f478eed685b13
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2023-04-02 15:36:17 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-
-nssm restart NekoRobot
+net stop Test_baba_bot
+net start Test_baba_bot
